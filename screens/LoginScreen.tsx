986e872ca63ba3069ae702aec5d0f2a3015bc5c4
abinash04-16 <abinash@francium.tech>
2024-06-04 10:23:12 +0530

    first commit

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,15 @@
+// components/Button.tsx
+
+import React from 'react';
+import {Text} from 'react-native';
+
+// Define the Button component
+const LoginScreen = () => {
+  return (
+    // <TouchableOpacity {...rest}>
+    <Text>login screen</Text>
+    // </TouchableOpacity>
+  );
+};
+
+export default LoginScreen;
